52af2858b020ae6d04bfa575288378e6fc12abbd
israelviana <rael232is@gmail.com>
2025-08-08 21:43:10 -0300

    chore: minor fix

--- cmd/api/rest/handler.go
+++ cmd/api/rest/handler.go
@@ -25,6 +25,6 @@ func (r *RestHandler) InitRestHandler() {
 
 func (r *RestHandler) Spiral(c *gin.Context) {
 	c.JSON(200, gin.H{
-		"message": "O símbolo da espiral é comumente considerado o sinal da vida e representa o ciclo da vida como um todo, desde o nascimento. vida, morte e, finalmente, renascimento. Use a espiral em seu ofício para reverenciar e honrar a vida, pois as espirais podem ser encontradas em muitos lugares da natureza (galáxias e conchas).",
+		"message": "JUNO - O símbolo da espiral é comumente considerado o sinal da vida e representa o ciclo da vida como um todo, desde o nascimento. vida, morte e, finalmente, renascimento. Use a espiral em seu ofício para reverenciar e honrar a vida, pois as espirais podem ser encontradas em muitos lugares da natureza (galáxias e conchas).",
 	})
 }
