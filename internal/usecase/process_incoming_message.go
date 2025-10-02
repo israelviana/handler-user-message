744abc5cfcee6fc383deeff1f6f99036fa288786
israelviana <rael232is@gmail.com>
2025-10-02 13:24:13 -0300

    chore: enhance error handling and logging in message processing

--- internal/usecase/process_incoming_message.go
+++ internal/usecase/process_incoming_message.go
@@ -3,7 +3,6 @@ package usecase
 import (
 	"context"
 	"errors"
-	"fmt"
 	"log"
 
 	"handler-user-message/internal/domain/clients/tito"
@@ -38,34 +37,36 @@ func (uc *ProcessIncomingMessageUseCase) Run(ctx context.Context, message string
 		return err
 	}
 
-	if res != nil {
-		m, ok := res.(map[string]interface{})
-		if !ok {
-			return errors.New("res is not a map[string]interface{}")
-		}
-
-		answer, ok := m["answer"].(string)
-		if ok {
-			fmt.Println(answer)
-		} else {
-			log.Println("answer is not string")
-		}
-
-		_, err = uc.whatsappClient.SendWhatsappMessage(ctx, whatsapp.MetaSendWhatsappMessageBody{
-			MessagingProduct: "whatsapp",
-			RecipientType:    "individual",
-			To:               sender,
-			Type:             "text",
-			Text: &whatsapp.MessageText{
-				PreviewUrl: false,
-				Body:       answer,
-			},
-		})
-		if err != nil {
-			log.Println(err)
-			return err
-		}
+	if res == nil {
+		return errors.New("error to call chat bot service")
 	}
 
-	return errors.New("error to call chat bot service")
+	m, ok := res.(map[string]interface{})
+	if !ok {
+		log.Println("chat bot response is not a map[string]interface{]", m)
+		return errors.New("res is not a map[string]interface{}")
+	}
+
+	answer, ok := m["answer"].(string)
+	if !ok {
+		log.Println("answer is not a valid string", m)
+		return errors.New("answer is not string")
+	}
+
+	_, err = uc.whatsappClient.SendWhatsappMessage(ctx, whatsapp.MetaSendWhatsappMessageBody{
+		MessagingProduct: "whatsapp",
+		RecipientType:    "individual",
+		To:               sender,
+		Type:             "text",
+		Text: &whatsapp.MessageText{
+			PreviewUrl: false,
+			Body:       answer,
+		},
+	})
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
 }
